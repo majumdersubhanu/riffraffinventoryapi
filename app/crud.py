ae90e29b9fdc3bac28ccbe7ac7088e711ad14377
subhanu-majumder <reach.subhanu@gmail.com>
2024-07-17 10:38:45 +0530

    refactor manual model creations using seattr

--- app/crud.py
+++ app/crud.py
@@ -143,36 +143,10 @@ class OrganizationRepo:
         db: Session,
         org: OrganizationCreateSchema,
     ):
-        org_in_db = OrganizationModel(
-            name=org.name,
-            fiscal_year_start_month=org.fiscal_year_start_month,
-            currency_code=org.currency_code,
-            time_zone=org.time_zone,
-            date_format=org.date_format,
-            field_separator=org.field_separator,
-            language_code=org.language_code,
-            industry_type=org.industry_type,
-            industry_size=org.industry_size,
-            portal_name=org.portal_name,
-            org_address=org.org_address,
-            remit_to_address=org.remit_to_address,
-            is_default_org=org.is_default_org,
-            account_created_date=org.account_created_date,
-            contact_name=org.contact_name,
-            company_id_label=org.company_id_label,
-            company_id_value=org.company_id_value,
-            tax_id_label=org.tax_id_label,
-            tax_id_value=org.tax_id_value,
-            currency_id=org.currency_id,
-            currency_symbol=org.currency_symbol,
-            currency_format=org.currency_format,
-            price_precision=org.price_precision,
-            phone=org.phone,
-            fax=org.fax,
-            website=org.website,
-            email=org.email,
-            is_org_active=org.is_org_active,
-        )
+        org_in_db = OrganizationModel()
+
+        for key, value in org:
+            setattr(org_in_db, key, value)
 
         db.add(org_in_db)
         db.commit()
@@ -181,15 +155,13 @@ class OrganizationRepo:
         # Adding addresses if any
         if org.addresses is not None:
             for addr in org.addresses:
-                db_addr = AddressModel(
-                    organization_id=org_in_db.id,
-                    street_address1=addr.street_address1,
-                    street_address2=addr.street_address2,
-                    city=addr.city,
-                    state=addr.state,
-                    country=addr.country,
-                    zip=addr.zip,
-                )
+                db_addr = AddressModel()
+
+                for key, value in addr:
+                    setattr(db_addr, key, value)
+
+                setattr(db_addr, "organization_id", org_in_db.id)
+
                 db.add(db_addr)
                 db.commit()
                 db.refresh(db_addr)
@@ -197,12 +169,13 @@ class OrganizationRepo:
         # Adding custom fields if any
         if org.custom_fields is not None:
             for custom_field in org.custom_fields:
-                db_custom_field = CustomFieldModel(
-                    organization_id=org_in_db.id,
-                    index=custom_field.index,
-                    value=custom_field.value,
-                    label=custom_field.label,
-                )
+                db_custom_field = CustomFieldModel()
+
+                for key, value in custom_field:
+                    setattr(db_custom_field, key, value)
+
+                setattr(db_custom_field, "organization_id", org_in_db.id)
+
                 db.add(db_custom_field)
                 db.commit()
                 db.refresh(db_custom_field)
